8671eaa64e1edb0ade4cc340ea9bcf73dff75edd
ZhongShengping <chdzsp@163.com>
2016-12-01 15:42:24 +0800

    Fix spec test for sync
    Change-Id: I2c5da6f0a030ef17b2907f1e0990c5b4ef241e08

--- spec/classes/aodh_db_sync_spec.rb
+++ spec/classes/aodh_db_sync_spec.rb
@@ -12,6 +12,8 @@ describe 'aodh::db::sync' do
         :path        => '/usr/bin',
         :refreshonly => 'true',
         :user        => 'aodh',
+        :try_sleep   => 5,
+        :tries       => 10,
         :logoutput   => 'on_failure',
         :subscribe   => ['Anchor[aodh::install::end]',
                          'Anchor[aodh::config::end]',
