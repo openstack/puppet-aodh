18db7cd4b4637c0f34771b4ddbe3e6c510639a88
ZhongShengping <chdzsp@163.com>
2018-03-14 10:37:11 +0800

    Switch acceptance tests to use integration classes
    Change-Id: I4f21dff29d557474302b233fc40e6c6bfc219987

--- spec/acceptance/aodh_wsgi_apache_spec.rb
+++ spec/acceptance/aodh_wsgi_apache_spec.rb
@@ -11,53 +11,7 @@ describe 'basic aodh' do
       include ::openstack_integration::rabbitmq
       include ::openstack_integration::mysql
       include ::openstack_integration::keystone
-
-      rabbitmq_user { 'aodh':
-        admin    => true,
-        password => 'an_even_bigger_secret',
-        provider => 'rabbitmqctl',
-        require  => Class['rabbitmq'],
-      }
-
-      rabbitmq_user_permissions { 'aodh@/':
-        configure_permission => '.*',
-        write_permission     => '.*',
-        read_permission      => '.*',
-        provider             => 'rabbitmqctl',
-        require              => Class['rabbitmq'],
-      }
-
-      class { '::aodh':
-        debug                 => true,
-        default_transport_url => 'rabbit://aodh:an_even_bigger_secret@127.0.0.1:5672',
-        database_connection   => 'mysql://aodh:a_big_secret@127.0.0.1/aodh?charset=utf8',
-      }
-      class { '::aodh::db::mysql':
-        password => 'a_big_secret',
-      }
-      class { '::aodh::keystone::auth':
-        password => 'a_big_secret',
-      }
-      class { '::aodh::keystone::authtoken':
-        password => 'a_big_secret',
-      }
-      class { '::aodh::api':
-        enabled      => true,
-        service_name => 'httpd',
-        sync_db      => true,
-      }
-      include ::apache
-      class { '::aodh::wsgi::apache':
-        ssl => false,
-      }
-      class { '::aodh::auth':
-        auth_url      => 'http://127.0.0.1:5000/v3',
-        auth_password => 'a_big_secret',
-      }
-      class { '::aodh::client': }
-      class { '::aodh::notifier': }
-      class { '::aodh::listener': }
-      class { '::aodh::evaluator': }
+      include ::openstack_integration::aodh
       EOS
 
 
