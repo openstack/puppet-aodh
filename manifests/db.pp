9b10d779b14daecd8772de338e31d4b6bb3c715e
ZhongShengping <chdzsp@163.com>
2019-04-28 10:58:49 +0800

    Deprecate idle_timeout option
    The idle_timeout parameter is deprecated, use connection_recycle_time
    instead[1].
    
    [1]https://review.opendev.org/#/c/334182/
    
    Change-Id: Iaed4d4795a7bfa5871209bcd3b4a6d793476ac31
    Depends-On: https://review.opendev.org/656106/
    Closes-Bug: #1826692

--- manifests/db.pp
+++ manifests/db.pp
@@ -17,7 +17,7 @@
 #   (optional) Connection url to connect to aodh slave database (read-only).
 #   Defaults to $::os_service_default.
 #
-# [*database_idle_timeout*]
+# [*database_connection_recycle_time*]
 #   Timeout when db connections should be reaped.
 #   (Optional) Defaults to $::os_service_default.
 #
@@ -46,40 +46,53 @@
 #   (Optional) If set, use this value for pool_timeout with SQLAlchemy.
 #   Defaults to $::os_service_default
 #
+# DEPRECATED PARAMETERS
+#
+# [*database_idle_timeout*]
+#   Timeout when db connections should be reaped.
+#   Defaults to undef.
+#
 class aodh::db (
-  $database_db_max_retries = $::os_service_default,
-  $database_connection     = 'sqlite:////var/lib/aodh/aodh.sqlite',
-  $slave_connection        = $::os_service_default,
-  $database_idle_timeout   = $::os_service_default,
-  $database_min_pool_size  = $::os_service_default,
-  $database_max_pool_size  = $::os_service_default,
-  $database_max_retries    = $::os_service_default,
-  $database_retry_interval = $::os_service_default,
-  $database_max_overflow   = $::os_service_default,
-  $database_pool_timeout   = $::os_service_default,
+  $database_db_max_retries          = $::os_service_default,
+  $database_connection              = 'sqlite:////var/lib/aodh/aodh.sqlite',
+  $slave_connection                 = $::os_service_default,
+  $database_connection_recycle_time = $::os_service_default,
+  $database_min_pool_size           = $::os_service_default,
+  $database_max_pool_size           = $::os_service_default,
+  $database_max_retries             = $::os_service_default,
+  $database_retry_interval          = $::os_service_default,
+  $database_max_overflow            = $::os_service_default,
+  $database_pool_timeout            = $::os_service_default,
+  # DEPRECATED PARAMETERS
+  $database_idle_timeout            = undef,
 ) {
 
   include ::aodh::deps
 
+  if $database_idle_timeout {
+    warning('The database_idle_timeout parameter is deprecated. Please use \
+database_connection_recycle_time instead.')
+  }
+
   $database_connection_real = pick($::aodh::database_connection, $database_connection)
   $slave_connection_real = pick($::aodh::slave_connection, $slave_connection)
-  $database_idle_timeout_real = pick($::aodh::database_idle_timeout, $database_idle_timeout)
   $database_min_pool_size_real = pick($::aodh::database_min_pool_size, $database_min_pool_size)
   $database_max_pool_size_real = pick($::aodh::database_max_pool_size, $database_max_pool_size)
   $database_max_retries_real = pick($::aodh::database_max_retries, $database_max_retries)
   $database_retry_interval_real = pick($::aodh::database_retry_interval, $database_retry_interval)
   $database_max_overflow_real = pick($::aodh::database_max_overflow, $database_max_overflow)
+  $database_connection_recycle_time_real = pick($::aodh::database_idle_timeout, $database_idle_timeout, $database_connection_recycle_time)
 
   oslo::db { 'aodh_config':
-    db_max_retries   => $database_db_max_retries,
-    connection       => $database_connection_real,
-    slave_connection => $slave_connection_real,
-    idle_timeout     => $database_idle_timeout_real,
-    min_pool_size    => $database_min_pool_size_real,
-    max_pool_size    => $database_max_pool_size_real,
-    max_retries      => $database_max_retries_real,
-    retry_interval   => $database_retry_interval_real,
-    max_overflow     => $database_max_overflow_real,
-    pool_timeout     => $database_pool_timeout,
+    db_max_retries          => $database_db_max_retries,
+    connection              => $database_connection_real,
+    slave_connection        => $slave_connection_real,
+    connection_recycle_time => $database_connection_recycle_time_real,
+    min_pool_size           => $database_min_pool_size_real,
+    max_pool_size           => $database_max_pool_size_real,
+    max_retries             => $database_max_retries_real,
+    retry_interval          => $database_retry_interval_real,
+    max_overflow            => $database_max_overflow_real,
+    pool_timeout            => $database_pool_timeout,
   }
 }
--- spec/classes/aodh_db_spec.rb
+++ spec/classes/aodh_db_spec.rb
@@ -6,47 +6,47 @@ describe 'aodh::db' do
       it { should contain_class('aodh::deps') }
 
       it { should contain_oslo__db('aodh_config').with(
-        :db_max_retries => '<SERVICE DEFAULT>',
-        :connection     => 'sqlite:////var/lib/aodh/aodh.sqlite',
-        :idle_timeout   => '<SERVICE DEFAULT>',
-        :min_pool_size  => '<SERVICE DEFAULT>',
-        :max_pool_size  => '<SERVICE DEFAULT>',
-        :max_retries    => '<SERVICE DEFAULT>',
-        :retry_interval => '<SERVICE DEFAULT>',
-        :max_overflow   => '<SERVICE DEFAULT>',
-        :pool_timeout   => '<SERVICE DEFAULT>',
+        :db_max_retries          => '<SERVICE DEFAULT>',
+        :connection              => 'sqlite:////var/lib/aodh/aodh.sqlite',
+        :connection_recycle_time => '<SERVICE DEFAULT>',
+        :min_pool_size           => '<SERVICE DEFAULT>',
+        :max_pool_size           => '<SERVICE DEFAULT>',
+        :max_retries             => '<SERVICE DEFAULT>',
+        :retry_interval          => '<SERVICE DEFAULT>',
+        :max_overflow            => '<SERVICE DEFAULT>',
+        :pool_timeout            => '<SERVICE DEFAULT>',
       )}
     end
 
     context 'with specific parameters' do
       let :params do
         {
-          :database_db_max_retries => '-1',
-          :database_connection     => 'mysql+pymysql://aodh:aodh@localhost/aodh',
-          :slave_connection        => 'mysql+pymysql://aodh:aodh@localhost2/aodh',
-          :database_idle_timeout   => '3601',
-          :database_min_pool_size  => '2',
-          :database_max_pool_size  => '11',
-          :database_max_retries    => '11',
-          :database_retry_interval => '11',
-          :database_max_overflow   => '21',
-          :database_pool_timeout   => '21',
+          :database_db_max_retries          => '-1',
+          :database_connection              => 'mysql+pymysql://aodh:aodh@localhost/aodh',
+          :slave_connection                 => 'mysql+pymysql://aodh:aodh@localhost2/aodh',
+          :database_connection_recycle_time => '3601',
+          :database_min_pool_size           => '2',
+          :database_max_pool_size           => '11',
+          :database_max_retries             => '11',
+          :database_retry_interval          => '11',
+          :database_max_overflow            => '21',
+          :database_pool_timeout            => '21',
         }
       end
 
       it { should contain_class('aodh::deps') }
 
       it { should contain_oslo__db('aodh_config').with(
-        :db_max_retries   => '-1',
-        :connection       => 'mysql+pymysql://aodh:aodh@localhost/aodh',
-        :slave_connection => 'mysql+pymysql://aodh:aodh@localhost2/aodh',
-        :idle_timeout     => '3601',
-        :min_pool_size    => '2',
-        :max_pool_size    => '11',
-        :max_retries      => '11',
-        :retry_interval   => '11',
-        :max_overflow     => '21',
-        :pool_timeout     => '21',
+        :db_max_retries          => '-1',
+        :connection              => 'mysql+pymysql://aodh:aodh@localhost/aodh',
+        :slave_connection        => 'mysql+pymysql://aodh:aodh@localhost2/aodh',
+        :connection_recycle_time => '3601',
+        :min_pool_size           => '2',
+        :max_pool_size           => '11',
+        :max_retries             => '11',
+        :retry_interval          => '11',
+        :max_overflow            => '21',
+        :pool_timeout            => '21',
       )}
     end
   end
