4861f818243bec06c8cd228c729b8d0985890b46
Takashi Kajinami <tkajinam@redhat.com>
2020-12-29 15:48:50 +0900

    Use yaml instead of json for policy file
    Because usage of json for policy file will be deprecated and replaced
    by yaml[1].
    
    [1] https://governance.openstack.org/tc/goals/selected/wallaby/migrate-policy-format-from-json-to-yaml.html
    
    Depends-on: https://review.opendev.org/769647
    Change-Id: I6f2a6a611487ca015eccf8cb91c1346647b8e52b

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -20,12 +20,12 @@
 #   Defaults to empty hash.
 #
 # [*policy_path*]
-#   (Optional) Path to the nova policy.json file
-#   Defaults to /etc/aodh/policy.json
+#   (Optional) Path to the nova policy.yaml file
+#   Defaults to /etc/aodh/policy.yaml
 #
 class aodh::policy (
   $policies    = {},
-  $policy_path = '/etc/aodh/policy.json',
+  $policy_path = '/etc/aodh/policy.yaml',
 ) {
 
   include aodh::deps
@@ -34,9 +34,10 @@ class aodh::policy (
   validate_legacy(Hash, 'validate_hash', $policies)
 
   Openstacklib::Policy::Base {
-    file_path  => $policy_path,
-    file_user  => 'root',
-    file_group => $::aodh::params::group,
+    file_path   => $policy_path,
+    file_user   => 'root',
+    file_group  => $::aodh::params::group,
+    file_format => 'yaml',
   }
 
   create_resources('openstacklib::policy::base', $policies)
