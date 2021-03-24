e11edd487cd380a8a257c4d5b14092b8d6eacdb5
Takashi Kajinami <tkajinam@redhat.com>
2021-03-24 12:23:15 +0900

    Add support for oslo_policy/enforce_new_defaults
    Depends-on: https://review.opendev.org/781428
    Change-Id: I64511ee42f7b88e1721bb4b6b963f6ea198dcbd7

--- spec/classes/aodh_policy_spec.rb
+++ spec/classes/aodh_policy_spec.rb
@@ -4,9 +4,10 @@ describe 'aodh::policy' do
   shared_examples 'aodh::policy' do
     let :params do
       {
-        :enforce_scope => false,
-        :policy_path   => '/etc/aodh/policy.yaml',
-        :policies      => {
+        :enforce_scope        => false,
+        :enforce_new_defaults => false,
+        :policy_path          => '/etc/aodh/policy.yaml',
+        :policies             => {
           'context_is_admin' => {
             'key'   => 'context_is_admin',
             'value' => 'foo:bar'
@@ -24,8 +25,9 @@ describe 'aodh::policy' do
         :file_format => 'yaml',
       })
       is_expected.to contain_oslo__policy('aodh_config').with(
-        :enforce_scope => false,
-        :policy_file   => '/etc/aodh/policy.yaml',
+        :enforce_scope        => false,
+        :enforce_new_defaults => false,
+        :policy_file          => '/etc/aodh/policy.yaml',
       )
     end
   end
