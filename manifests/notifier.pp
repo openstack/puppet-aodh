86aa1e34e9853ff731d45ed92327c1b4df0ae771
Takashi Kajinami <tkajinam@redhat.com>
2021-04-23 22:57:38 +0900

    Support batch_* parameters of notifier
    This change introduces support for batch_* parameters in notifier,
    which are tunable parameters about batch processing of alarms.
    
    Change-Id: I8beee8a794b6607875fbc7a46ccc1e58c0c344b5

--- manifests/notifier.pp
+++ manifests/notifier.pp
@@ -17,18 +17,32 @@
 #    (optional) Number of workers for notifier service.
 #    Defaults to $::os_workers.
 #
+#  [*batch_size*]
+#    (optional) Number of notification messages to wait before dispatching
+#    them.
+#    Defaults to $::os_service_default.
+#
+#  [*batch_timeout*]
+#    (optional) Number of seconds to wait before dispatching samples when
+#    batch_size is not reached.
+#    Defaults to $::os_service_default
+#
 class aodh::notifier (
   $manage_service = true,
   $enabled        = true,
   $package_ensure = 'present',
   $workers        = $::os_workers,
+  $batch_size     = $::os_service_default,
+  $batch_timeout  = $::os_service_default,
 ) {
 
   include aodh::deps
   include aodh::params
 
   aodh_config {
-    'notifier/workers': value => $workers;
+    'notifier/workers':       value => $workers;
+    'notifier/batch_size':    value => $batch_size;
+    'notifier/batch_timeout': value => $batch_timeout
   }
 
   ensure_resource( 'package', [$::aodh::params::notifier_package_name],
