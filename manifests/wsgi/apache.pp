c8c59f1e9aabffeceaf85ead3590eea90ee98d86
Takashi Kajinami <tkajinam@redhat.com>
2022-08-26 14:29:55 +0900

    Add Apache WSGI logging parameters for pipe/syslog
    Add parameters for advanced logging configurations in Apache to
    support piped logging and support for syslog (via mod_syslog
    available in Apache >= 2.5.0)
    
    Co-Authored-By: Andy Botting <andy@andybotting.com>
    Change-Id: I16c7a6407d647a25d6093239b30845a212202a5c

--- manifests/wsgi/apache.pp
+++ manifests/wsgi/apache.pp
@@ -74,6 +74,14 @@
 #     The log file name for the virtualhost.
 #     Optional. Defaults to undef.
 #
+#   [*access_log_pipe*]
+#     Specifies a pipe where Apache sends access logs for the virtualhost.
+#     Optional. Defaults to undef.
+#
+#   [*access_log_syslog*]
+#     Sends the virtualhost access log messages to syslog.
+#     Optional. Defaults to undef.
+#
 #   [*access_log_format*]
 #     The log format for the virtualhost.
 #     Optional. Defaults to undef.
@@ -82,13 +90,21 @@
 #     The error log file name for the virtualhost.
 #     Optional. Defaults to undef.
 #
-#  [*custom_wsgi_process_options*]
-#    (optional) gives you the opportunity to add custom process options or to
-#    overwrite the default options for the WSGI main process.
-#    eg. to use a virtual python environment for the WSGI process
-#    you could set it to:
-#    { python-path => '/my/python/virtualenv' }
-#    Defaults to {}
+#   [*error_log_pipe*]
+#     Specifies a pipe where Apache sends error logs for the virtualhost.
+#     Optional. Defaults to undef.
+#
+#   [*error_log_syslog*]
+#     Sends the virtualhost error log messages to syslog.
+#     Optional. Defaults to undef.
+#
+#   [*custom_wsgi_process_options*]
+#     (optional) gives you the opportunity to add custom process options or to
+#     overwrite the default options for the WSGI main process.
+#     eg. to use a virtual python environment for the WSGI process
+#     you could set it to:
+#     { python-path => '/my/python/virtualenv' }
+#     Defaults to {}
 #
 #   [*wsgi_script_dir*]
 #     (optional) The directory to install the WSGI script for apache to read
@@ -140,8 +156,12 @@ class aodh::wsgi::apache (
   $threads                     = 1,
   $priority                    = 10,
   $access_log_file             = undef,
+  $access_log_pipe             = undef,
+  $access_log_syslog           = undef,
   $access_log_format           = undef,
   $error_log_file              = undef,
+  $error_log_pipe              = undef,
+  $error_log_syslog            = undef,
   $custom_wsgi_process_options = {},
   $wsgi_script_dir             = $::aodh::params::aodh_wsgi_script_dir,
   $wsgi_script_source          = $::aodh::params::aodh_wsgi_script_source,
@@ -185,7 +205,11 @@ class aodh::wsgi::apache (
     request_headers             => $request_headers,
     custom_wsgi_process_options => $custom_wsgi_process_options,
     access_log_file             => $access_log_file,
+    access_log_pipe             => $access_log_pipe,
+    access_log_syslog           => $access_log_syslog,
     access_log_format           => $access_log_format,
     error_log_file              => $error_log_file,
+    error_log_pipe              => $error_log_pipe,
+    error_log_syslog            => $error_log_syslog,
   }
 }
