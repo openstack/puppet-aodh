87ea30deed1594a8bb6fd7cdf31164f79d40b30c
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-09-09 16:52:43 +0900

    Add support for [oslo_messaging_rabbit] enable_cancel_on_failover
    Depends-on: https://review.opendev.org/928440
    Change-Id: Iaf297c332269f6247b6f698aa9e82c4f5b303db0

--- manifests/init.pp
+++ manifests/init.pp
@@ -88,6 +88,11 @@
 #   (Optional) Limit the number of memory bytes used by the quorum queue.
 #   Defaults to $facts['os_service_default']
 #
+# [*enable_cancel_on_failover*]
+#   (Optional) Enable x-cancel-on-ha-failover flag so that rabbitmq server will
+#   cancel and notify consumers when queue is down.
+#   Defaults to $facts['os_service_default']
+#
 # [*kombu_ssl_ca_certs*]
 #   (optional) SSL certification authority file (valid only if SSL enabled).
 #   Defaults to $facts['os_service_default']
@@ -164,6 +169,7 @@ class aodh (
   $rabbit_quorum_delivery_limit       = $facts['os_service_default'],
   $rabbit_quorum_max_memory_length    = $facts['os_service_default'],
   $rabbit_quorum_max_memory_bytes     = $facts['os_service_default'],
+  $enable_cancel_on_failover          = $facts['os_service_default'],
   $kombu_ssl_ca_certs                 = $facts['os_service_default'],
   $kombu_ssl_certfile                 = $facts['os_service_default'],
   $kombu_ssl_keyfile                  = $facts['os_service_default'],
@@ -210,6 +216,7 @@ class aodh (
     rabbit_quorum_delivery_limit    => $rabbit_quorum_delivery_limit,
     rabbit_quorum_max_memory_length => $rabbit_quorum_max_memory_length,
     rabbit_quorum_max_memory_bytes  => $rabbit_quorum_max_memory_bytes,
+    enable_cancel_on_failover       => $enable_cancel_on_failover,
   }
 
   oslo::messaging::default { 'aodh_config':
--- spec/classes/aodh_init_spec.rb
+++ spec/classes/aodh_init_spec.rb
@@ -47,6 +47,7 @@ describe 'aodh' do
           :rabbit_quorum_delivery_limit    => '<SERVICE DEFAULT>',
           :rabbit_quorum_max_memory_length => '<SERVICE DEFAULT>',
           :rabbit_quorum_max_memory_bytes  => '<SERVICE DEFAULT>',
+          :enable_cancel_on_failover       => '<SERVICE DEFAULT>',
         )
         is_expected.to contain_oslo__messaging__notifications('aodh_config').with(
           :transport_url => '<SERVICE DEFAULT>',
@@ -90,6 +91,7 @@ describe 'aodh' do
           :rabbit_quorum_delivery_limit       => 3,
           :rabbit_quorum_max_memory_length    => 5,
           :rabbit_quorum_max_memory_bytes     => 1073741824,
+          :enable_cancel_on_failover          => false,
           :notification_transport_url         => 'rabbit://rabbit_user:password@localhost:5673',
           :notification_driver                => 'ceilometer.compute.aodh_notifier',
           :notification_topics                => 'openstack',
@@ -123,6 +125,7 @@ describe 'aodh' do
           :rabbit_quorum_delivery_limit    => 3,
           :rabbit_quorum_max_memory_length => 5,
           :rabbit_quorum_max_memory_bytes  => 1073741824,
+          :enable_cancel_on_failover       => false,
         )
         is_expected.to contain_oslo__messaging__notifications('aodh_config').with(
           :transport_url => 'rabbit://rabbit_user:password@localhost:5673',
